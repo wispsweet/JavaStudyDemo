7fbd36c12aab1c6ea7678e9c0ba3f8e80d513dac
yanhaochuang <yanhaochuang@qq.com>
2020-08-26 13:39:38 +0800

    新增注释

--- src/main/java/com/study/first/receiver/DirectReceiver.java
+++ src/main/java/com/study/first/receiver/DirectReceiver.java
@@ -10,7 +10,7 @@ import java.util.Map;
 /**
  * User: YHC
  * Date: 2020/8/21 13:57
- * DESC: DirectExchange主题交换机模式|点对点模式
+ * DESC: DirectExchange主题交换机模式|点对点模式|可以创建多个listener监听同一个队列，当前消费端轮询方式获取消息
  */
 @Component
 @RabbitListener(queuesToDeclare = @Queue("TestDirectQueue")) //监听队列名称
